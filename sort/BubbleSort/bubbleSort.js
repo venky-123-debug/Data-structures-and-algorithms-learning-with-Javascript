97ad5cb59bbd17afa7d708888613a83d4201c45d
Venkatesh <venkat@print2block.com>
2025-04-23 22:26:40 +0530

    sepeate folder created for bubble sort algo

---
+++ sort/BubbleSort/bubbleSort.js
@@ -0,0 +1,35 @@
+/** In Ascending Order */
+const bubbleSort = (arr) => {
+  try {
+    let len = arr.length
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]] // array destructuring
+        }
+      }
+    }
+    return arr
+  } catch (error) {
+    console.error(error)
+  }
+}
+/** In Descending Order */
+const DescendingBubbleSort = (arr) => {
+  try {
+    let len = arr.length
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        if (arr[j] < arr[j + 1]) {
+          ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]] // array destructuring
+        }
+      }
+    }
+    return arr
+  } catch (error) {
+    console.error(error)
+  }
+}
+let arr = [64, 34, 25, 12, 22, 11, 90]
+console.log(bubbleSort(arr))
+console.log(DescendingBubbleSort(arr))
--- sort/readme.md
+++ sort/readme.md
@@ -1,15 +1,6 @@
-## Bubble Sort
+<!-- INTRODUCTION -->
 
-Description: A simple comparison-based sorting algorithm. It repeatedly swaps adjacent elements if they are in the wrong order.
-`Time Complexity:`
-Best Case: O(n) (when the array is already sorted)
-Worst Case: O(n²)
-Space Complexity: O(1)
-
-# example:
-
-1. sorting students by height,
-2. arranging book in shelfe by size.
+Sorting is an important operation in computer science that arranges elements of an array or list in a certain order (either ascending or descending).
 
 ## Insertion Sort
 
